7a82a4cb874cba32d4e7e6ea1b2f8bf60a786650
Neesh <avaneeshsamsi@gmail.com>
2021-11-10 15:53:45 +0530

    feat: Nav basic html

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -7,7 +7,29 @@ import Instagram from "../../assets/socials/instagram.svg";
 import YouTube from "../../assets/socials/youtube.svg";
 
 const NavBar = () => {
-  return <div></div>;
+  return (
+    <nav className="">
+      <ul className="">
+        <li>Home</li>
+        <li>About</li>
+        <li>Gallery</li>
+        <li>Schedule</li>
+        <li>SaathSangath</li>
+        <li>Contact</li>
+      </ul>
+      <ul>
+        <a href="https://fb.me/PanditYogeshSamsi">
+          <Image src={Facebook} alt="Yogesh Samsi Facebook Page (Facebook Logo)" target="_blank" />
+        </a>
+        <a href="https://instagram.com/yogesh.samsi">
+          <Image src={Instagram} alt="Yogesh Samsi Instgram Account (Instagram Logo)" target="_blank" />
+        </a>
+        <a href="https://youtube.com/YogeshSamsiOfficial">
+          <Image src={YouTube} alt="Yogesh Samsi YouTube Channel (YouTube Logo)" target="_blank" />
+        </a>
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
