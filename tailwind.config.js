8b40f06a1d9f1498de84913cd7e391c7b7210f90
Neesh <avaneeshsamsi@gmail.com>
2021-11-10 15:15:51 +0530

    feat: tailwind custom colors config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    // colors: {},
+    extend: {
+      colors: {
+        dark: "#222222",
+        light: "#F4EDE1",
+        brown: "#362009",
+        cream: "#E9C493",
+      },
+    },
   },
   variants: {
     extend: {},
