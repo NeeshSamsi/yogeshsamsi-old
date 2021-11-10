42dc182414a97a02b613bd263ff6885483fc7cc0
Neesh <avaneeshsamsi@gmail.com>
2021-11-10 15:51:21 +0530

    feat: components file structure

---
+++ components/NavBar/index.js
@@ -0,0 +1,13 @@
+// NEXT
+import Image from "next/image";
+
+// Images / Icons
+import Facebook from "../../assets/socials/facebook.svg";
+import Instagram from "../../assets/socials/instagram.svg";
+import YouTube from "../../assets/socials/youtube.svg";
+
+const NavBar = () => {
+  return <div></div>;
+};
+
+export default NavBar;
