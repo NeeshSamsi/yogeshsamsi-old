4038354203f1b987212bb7efc3bb23a19cad419d
Neesh <avaneeshsamsi@gmail.com>
2021-11-11 16:54:11 +0530

    feat: added keywords meta tag

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,4 +1,5 @@
 // NEXT
+import Link from "next/link";
 import Image from "next/image";
 
 // Images / Icons
@@ -8,45 +9,44 @@ import YouTube from "../../assets/socials/youtube.svg";
 
 const NavBar = ({ bg }) => {
   return (
-    <nav>
-      <div>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Gallery</li>
-          <li>Schedule</li>
-          <li>SaathSangath</li>
-          <li>Contact</li>
-        </ul>
-        <ul>
+    <nav className={`bg-${bg} w-full`}>
+      <div className="max-w-[80%] mx-auto flex justify-between">
+        <div>
+          <Link href="/">
+            <a> Home</a>
+          </Link>
+          <Link href="/">
+            <a> About</a>
+          </Link>
+          <Link href="/">
+            <a> Gallery</a>
+          </Link>
+          <Link href="/">
+            <a> Schedule</a>
+          </Link>
+          <Link href="/">
+            <a> SaathSangath</a>
+          </Link>
+          <Link href="/">
+            <a> Contact</a>
+          </Link>
+        </div>
+        <div>
           <a href="https://fb.me/PanditYogeshSamsi">
-            <Image
-              src={Facebook}
-              alt="Yogesh Samsi Facebook Page (Facebook Logo)"
-              target="_blank"
-              width="100%"
-              height="100%"
-            />
+            <Image src={Facebook} alt="Yogesh Samsi Facebook Page (Facebook Logo)" target="_blank" className="image" />
           </a>
           <a href="https://instagram.com/yogesh.samsi">
             <Image
               src={Instagram}
               alt="Yogesh Samsi Instgram Account (Instagram Logo)"
               target="_blank"
-              width="100%"
-              height="100%"
+              className="image"
             />
           </a>
           <a href="https://youtube.com/YogeshSamsiOfficial">
-            <Image
-              src={YouTube}
-              alt="Yogesh Samsi YouTube Channel (YouTube Logo)"
-              target="_blank"
-              width="100%"
-              height="100%"
-            />
+            <Image src={YouTube} alt="Yogesh Samsi YouTube Channel (YouTube Logo)" target="_blank" className="image" />
           </a>
-        </ul>
+        </div>
       </div>
     </nav>
   );
