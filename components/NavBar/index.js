d8aad886591a38fb008d59c017ce0cda6d00da9b
Neesh Samsi <avaneeshsamsi@gmail.com>
2021-11-12 00:31:52 +0530

    feat: responsive nav - mob nav menu left

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -1,3 +1,6 @@
+// REACT
+import { useState } from "react";
+
 // NEXT
 import Link from "next/link";
 import Image from "next/image";
@@ -10,71 +13,85 @@ import { MenuIcon } from "@heroicons/react/outline";
 import { XIcon } from "@heroicons/react/outline";
 
 const NavBar = ({ bg }) => {
-  return (
-    <>
-      <FullNav bg={bg} />
+  const [isHamburgerOpen, setIsHamburgerOpen] = useState(false);
 
-      <HamburgerNav bg={bg} />
-    </>
+  return (
+    <nav className={`fixed top-0 left-0 bg-${bg} w-full py-2 md:py-4 text-light  lg:text-lg 2xl:text-xl`}>
+      {/* main nav container */}
+      {/* mobile mw-90%, 90% | desktop mw-80%, {TBD} */}
+      <div className="max-w-[80%] mx-auto flex justify-between items-center">
+        {/* LEFT */}
+        {/* desktop links */}
+        <div className="hidden md:flex justify-between md:space-x-4 lg:space-x-6">
+          <Link href="/">
+            <a> Home</a>
+          </Link>
+          <Link href="/">
+            <a> About</a>
+          </Link>
+          <Link href="/">
+            <a> Gallery</a>
+          </Link>
+          <Link href="/">
+            <a> Schedule</a>
+          </Link>
+          <Link href="/">
+            <a> SaathSangath</a>
+          </Link>
+          <Link href="/">
+            <a> Contact</a>
+          </Link>
+        </div>
+        {/* mobile logo font */}
+        <Link href="/">
+          <a>
+            <h1 className="block md:hidden font-serif font-bold text-xl">Yogesh Samsi</h1>
+          </a>
+        </Link>
+
+        {/* LEFT */}
+        {/* destop socials */}
+        <div className="hidden md:flex justify-between items-center md:space-x-2 lg:space-x-4">
+          <a href="https://fb.me/PanditYogeshSamsi">
+            <div className="w-8 h-8 md:w-6 md:h-6 lg:w-7 lg:h-7">
+              <Image
+                src={Facebook}
+                alt="Yogesh Samsi Facebook Page (Facebook Logo)"
+                target="_blank"
+                className="image"
+              />
+            </div>
+          </a>
+          <a href="https://instagram.com/yogesh.samsi">
+            <div className="w-8 h-8 md:w-6 md:h-6 lg:w-7 lg:h-7">
+              <Image
+                src={Instagram}
+                alt="Yogesh Samsi Instgram Account (Instagram Logo)"
+                target="_blank"
+                className="image"
+              />
+            </div>
+          </a>
+          <a href="https://youtube.com/YogeshSamsiOfficial">
+            <div className="w-8 h-8 md:w-6 md:h-6 lg:w-7 lg:h-7">
+              <Image
+                src={YouTube}
+                alt="Yogesh Samsi YouTube Channel (YouTube Logo)"
+                target="_blank"
+                className="image"
+              />
+            </div>
+          </a>
+        </div>
+        {/* mobile hamburger */}
+        {isHamburgerOpen ? (
+          <XIcon className="block md:hidden w-8 h-8" />
+        ) : (
+          <MenuIcon className="block md:hidden w-8 h-8" />
+        )}
+      </div>
+    </nav>
   );
 };
-
-const FullNav = ({ bg }) => (
-  <nav className={`fixed top-0 left-0 bg-dark-30 w-full text-light 2xl:text-xl xl:text-lg lg:text-lg lgmax:hidden`}>
-    <div className="max-w-[80%] mx-auto py-4 flex justify-between items-center">
-      <div className="flex justify-between space-x-6">
-        <Link href="/">
-          <a> Home</a>
-        </Link>
-        <Link href="/">
-          <a> About</a>
-        </Link>
-        <Link href="/">
-          <a> Gallery</a>
-        </Link>
-        <Link href="/">
-          <a> Schedule</a>
-        </Link>
-        <Link href="/">
-          <a> SaathSangath</a>
-        </Link>
-        <Link href="/">
-          <a> Contact</a>
-        </Link>
-      </div>
-
-      <div className="flex justify-between items-center space-x-4">
-        <a href="https://fb.me/PanditYogeshSamsi">
-          <div className="w-8 h-8 lg:w-7 lg:h7">
-            <Image src={Facebook} alt="Yogesh Samsi Facebook Page (Facebook Logo)" target="_blank" className="image" />
-          </div>
-        </a>
-        <a href="https://instagram.com/yogesh.samsi">
-          <div className="w-8 h-8 lg:w-7 lg:h7">
-            <Image
-              src={Instagram}
-              alt="Yogesh Samsi Instgram Account (Instagram Logo)"
-              target="_blank"
-              className="image"
-            />
-          </div>
-        </a>
-        <a href="https://youtube.com/YogeshSamsiOfficial">
-          <div className="w-8 h-8 lg:w-7 lg:h7">
-            <Image src={YouTube} alt="Yogesh Samsi YouTube Channel (YouTube Logo)" target="_blank" className="image" />
-          </div>
-        </a>
-      </div>
-    </div>
-  </nav>
-);
-
-const HamburgerNav = ({ bg }) => (
-  <nav className="lg:hidden flex justify-between">
-    <h1>Yogesh Samsi</h1>
-    <XIcon />
-    <MenuIcon />
-  </nav>
-);
 
 export default NavBar;
