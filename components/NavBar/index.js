a6f73c2f48c3eeacaefbb760be367933440c901b
Neesh <avaneeshsamsi@gmail.com>
2021-11-11 22:18:18 +0530

    feat: responsive nav func, mobile styles left

--- components/NavBar/index.js
+++ components/NavBar/index.js
@@ -9,64 +9,68 @@ import YouTube from "../../assets/socials/youtube.svg";
 
 const NavBar = ({ bg }) => {
   return (
-    <nav className={`sticky top-0 left-0 bg-${bg} w-full text-light text-xl`}>
-      <div className="max-w-[80%] mx-auto py-4 flex justify-between items-center">
-        <div className="w-[50%] flex justify-between">
-          <Link href="/">
-            <a> Home</a>
-          </Link>
-          <Link href="/">
-            <a> About</a>
-          </Link>
-          <Link href="/">
-            <a> Gallery</a>
-          </Link>
-          <Link href="/">
-            <a> Schedule</a>
-          </Link>
-          <Link href="/">
-            <a> SaathSangath</a>
-          </Link>
-          <Link href="/">
-            <a> Contact</a>
-          </Link>
-        </div>
+    <>
+      <FullNav bg={bg} />
 
-        <div className="w-[10%] flex justify-between items-center">
-          <a href="https://fb.me/PanditYogeshSamsi">
-            <div className="w-8 h-8">
-              <Image
-                src={Facebook}
-                alt="Yogesh Samsi Facebook Page (Facebook Logo)"
-                target="_blank"
-                className="image"
-              />
-            </div>
-          </a>
-          <a href="https://instagram.com/yogesh.samsi">
-            <div className="w-8 h-8">
-              <Image
-                src={Instagram}
-                alt="Yogesh Samsi Instgram Account (Instagram Logo)"
-                target="_blank"
-                className="image"
-              />
-            </div>
-          </a>
-          <a href="https://youtube.com/YogeshSamsiOfficial">
-            <div className="w-8 h-8">
-              <Image
-                src={YouTube}
-                alt="Yogesh Samsi YouTube Channel (YouTube Logo)"
-                target="_blank"
-                className="image"
-              />
-            </div>
-          </a>
-        </div>
-      </div>
-    </nav>
+      <HamburgerNav bg={bg} />
+    </>
   );
 };
+
+const FullNav = ({ bg }) => (
+  <nav className={`sticky top-0 left-0 bg-dark-30 w-full text-light 2xl:text-xl xl:text-lg lg:text-lg lgmax:hidden`}>
+    <div className="max-w-[80%] mx-auto py-4 flex justify-between items-center">
+      <div className="w-[60%] flex justify-between">
+        <Link href="/">
+          <a> Home</a>
+        </Link>
+        <Link href="/">
+          <a> About</a>
+        </Link>
+        <Link href="/">
+          <a> Gallery</a>
+        </Link>
+        <Link href="/">
+          <a> Schedule</a>
+        </Link>
+        <Link href="/">
+          <a> SaathSangath</a>
+        </Link>
+        <Link href="/">
+          <a> Contact</a>
+        </Link>
+      </div>
+
+      <div className="xl:w-[10%] lg:w-[12%] flex justify-between items-center">
+        <a href="https://fb.me/PanditYogeshSamsi">
+          <div className="w-8 h-8 lg:w-7 lg:h7">
+            <Image src={Facebook} alt="Yogesh Samsi Facebook Page (Facebook Logo)" target="_blank" className="image" />
+          </div>
+        </a>
+        <a href="https://instagram.com/yogesh.samsi">
+          <div className="w-8 h-8 lg:w-7 lg:h7">
+            <Image
+              src={Instagram}
+              alt="Yogesh Samsi Instgram Account (Instagram Logo)"
+              target="_blank"
+              className="image"
+            />
+          </div>
+        </a>
+        <a href="https://youtube.com/YogeshSamsiOfficial">
+          <div className="w-8 h-8 lg:w-7 lg:h7">
+            <Image src={YouTube} alt="Yogesh Samsi YouTube Channel (YouTube Logo)" target="_blank" className="image" />
+          </div>
+        </a>
+      </div>
+    </div>
+  </nav>
+);
+
+const HamburgerNav = ({ bg }) => (
+  <nav className="lg:hidden">
+    <h1>Mobile Nav</h1>
+  </nav>
+);
 
 export default NavBar;
