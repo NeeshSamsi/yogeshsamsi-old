9717158ffd8ccfc343e23df8c34c080591da36a8
Neesh <avaneeshsamsi@gmail.com>
2021-11-17 19:49:01 +0530

    feat: Icons into React Components

---
+++ components/Icons/RightArrow.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RightArrow = ({ background, foreground }) => {
+  return (
+    <svg width="14" height="22" viewBox="0 0 14 22" fill={background} xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 2.726L9.10231 11L0 19.274L2.44884 21.5L14 11L2.44884 0.5L0 2.726Z" fill={foreground} />
+    </svg>
+  );
+};
+
+export default RightArrow;
