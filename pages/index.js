1fb4b529060935fdd435bb331b2da33cc809b436
Neesh <avaneeshsamsi@gmail.com>
2021-11-17 19:49:54 +0530

    feat: home containers import and render

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,18 @@
 import HeadComponent from "../components/HeadComponent";
 import NavBar from "../components/NavBar";
 
+import { Hero, About, Schedule, MediaTestimonials } from "../containers/Home";
+
 const Home = () => {
   return (
     <>
       <HeadComponent />
 
       <NavBar theme={{ background: "dark", foreground: "light" }} />
+      <Hero />
+      <About />
+      <Schedule />
+      <MediaTestimonials />
     </>
   );
 };
