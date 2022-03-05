e3c58d9c837e2512df64b550328789f1c28b39c3
Neesh <avaneeshsamsi@gmail.com>
2022-03-05 11:31:32 +0530

    fix: Remove centering from desktop views

--- containers/Home/Hero.jsx
+++ containers/Home/Hero.jsx
@@ -16,11 +16,11 @@ const Hero = () => {
         mobileSrc="/img/home/hero-mobile.jpg"
       />
       <main className="absolute top-0 left-0 mx-auto h-screen w-full lg:mx-0 lg:flex lg:items-center lg:px-col-main">
-        <div className="mx-auto mt-16 flex max-w-2xl flex-col space-y-10 xs:space-y-10 sm:space-y-12 md:space-y-14 lg:space-y-20">
+        <div className="mx-auto mt-16 flex max-w-2xl flex-col space-y-10 xs:space-y-10 sm:space-y-12 md:space-y-14 lg:mx-0 lg:mt-0 lg:space-y-20">
           <h1 className="text-center font-serif text-5xl font-bold text-light md:text-7xl lg:text-left 2xl:text-8xl">
             Yogesh Samsi
           </h1>
-          <div className="flex w-full flex-col items-center justify-center xs:space-x-2 sm:flex-row sm:space-x-6 xl:text-xl 2xl:text-2xl">
+          <div className="flex w-full flex-col items-center justify-center xs:space-x-2 sm:flex-row sm:space-x-6 lg:justify-start xl:text-xl 2xl:text-2xl">
             <button className="flex items-center space-x-4 rounded-md bg-light p-4 text-sm font-semibold uppercase text-darker xs:p-2 sm:p-2 md:p-3 lg:p-4 lg:text-base xl:p-4 2xl:p-5">
               <span>Upcoming Events</span>
               <RightArrow foreground="#362009" />
