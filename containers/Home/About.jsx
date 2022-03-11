c1e95d18312ed4a2ad6ab6d7a0565c0cde83d8a1
Neesh <avaneeshsamsi@gmail.com>
2022-03-11 19:36:34 +0530

    feat: HTML & Mobile styling

--- containers/Home/About.jsx
+++ containers/Home/About.jsx
@@ -1,7 +1,39 @@
 import React from "react"
 
 const About = () => {
-  return <main>About</main>
+  return (
+    <section
+      role="About"
+      className="bg-lighter px-col-all text-darker md:px-col-main"
+    >
+      <h3 className="py-12 text-center font-serif text-xl font-medium">
+        A unique tabla artist who has simultaneously scaled unprecedented
+        heights of achievement in solo performance, accompaniment and pedagogy.
+      </h3>
+
+      <div className="mb-8 flex flex-col space-y-2 text-base">
+        <p className="font-semibold">As a Soloist,</p>
+        <p>
+          Yogesh balances both tradition and innovation in the Punjab Gharana.
+          His performances showcase mastery over a vast range of traditional
+          content, more recently unearthing and sharing rare repertoire that was
+          lost during partition. At the same time, he brings fresh perspective
+          to the canon by presenting new ideas that capture the philosophy and
+          rhythmic thought processes of his Guru, Ustad Allah Rakha.
+        </p>
+      </div>
+
+      <div className="flex flex-col space-y-2 pb-16 text-base">
+        <p className="font-semibold">As an Accompanist,</p>
+        <p>
+          Yogesh has provided sensitive and thoughtful tabla accompaniment for
+          many leading vocalists and instrumentalists. His approach has been
+          instilled by the close grooming under his father and stalwart
+          vocalist, Pandit Dinkar Kaikini.
+        </p>
+      </div>
+    </section>
+  )
 }
 
 export default About
