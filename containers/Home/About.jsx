38e640f246706e245bf965adadfc50f32e46f90f
Neesh <avaneeshsamsi@gmail.com>
2022-03-11 22:08:32 +0530

    fix: responsive text fixes

--- containers/Home/About.jsx
+++ containers/Home/About.jsx
@@ -6,14 +6,14 @@ const About = () => {
       role="About"
       className="bg-lighter px-col-all text-darker md:px-col-main"
     >
-      <h3 className="py-12 text-center font-serif text-xl font-medium">
+      <h3 className="py-16 text-center font-serif text-xl font-medium md:text-2xl xl:py-24 xl:text-3xl">
         A unique tabla artist who has simultaneously scaled unprecedented
         heights of achievement in solo performance, accompaniment and pedagogy.
       </h3>
 
-      <div className="mb-8 flex flex-col space-y-2 text-base">
+      <div className="xl: mb-12 flex flex-col space-y-4 text-base md:text-lg xl:mb-16 xl:flex-row xl:justify-between xl:space-y-0 xl:text-xl">
         <p className="font-semibold">As a Soloist,</p>
-        <p>
+        <p className="max-w-none xl:max-w-lg 2xl:max-w-3xl">
           Yogesh balances both tradition and innovation in the Punjab Gharana.
           His performances showcase mastery over a vast range of traditional
           content, more recently unearthing and sharing rare repertoire that was
@@ -23,9 +23,9 @@ const About = () => {
         </p>
       </div>
 
-      <div className="flex flex-col space-y-2 pb-16 text-base">
+      <div className="flex flex-col space-y-4 pb-24 text-base md:text-lg xl:flex-row xl:justify-between xl:text-xl">
         <p className="font-semibold">As an Accompanist,</p>
-        <p>
+        <p className="max-w-none xl:max-w-lg 2xl:max-w-3xl">
           Yogesh has provided sensitive and thoughtful tabla accompaniment for
           many leading vocalists and instrumentalists. His approach has been
           instilled by the close grooming under his father and stalwart
